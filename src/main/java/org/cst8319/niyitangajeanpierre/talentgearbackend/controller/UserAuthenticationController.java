35c45c66a97137d3f871d559c92f4539f89b5044
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2024-12-01 21:03:35 -0500

    register endpoint and use encrypted password

--- src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/controller/UserAuthenticationController.java
+++ src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/controller/UserAuthenticationController.java
@@ -3,10 +3,15 @@ package org.cst8319.niyitangajeanpierre.talentgearbackend.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.Dto.UserLoginDto;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.Dto.UserRegisterDto;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.entity.UserEntity;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.service.UserAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +46,19 @@ public class UserAuthenticationController {
         return "Logged out successfully";  // Client will discard the JWT token
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegisterDto userRegisterDto) {
+        try {
+         UserEntity registeredUser = userAuthenticationService.createUser(userRegisterDto);
 
-
-
+         // Authenticate the user
+            UserLoginDto userLoginDto = new UserLoginDto();
+            userLoginDto.setUsername(registeredUser.getUsername());
+            userLoginDto.setPassword(registeredUser.getPassword());
+            String jwt = userAuthenticationService.authenticateUser(userLoginDto);
+         return ResponseEntity.status(HttpStatus.CREATED).body(jwt);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while registering user");
+        }
+    }
 }
--- src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/service/UserAuthenticationService.java
+++ src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/service/UserAuthenticationService.java
@@ -3,14 +3,30 @@ package org.cst8319.niyitangajeanpierre.talentgearbackend.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.Dto.UserLoginDto;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.Dto.UserRegisterDto;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.config.CustomUserDetailsService;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.config.JwtAuthentication;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.config.JwtUtil;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.entity.RoleEntity;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.entity.UserEntity;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.repository.RoleRepository;
+import org.cst8319.niyitangajeanpierre.talentgearbackend.repository.UserRepository;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +34,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserAuthenticationService {
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtUtil jwtUtil;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public String authenticateUser(@RequestBody UserLoginDto userLoginDto) {
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(userLoginDto.getUsername());
@@ -25,11 +45,12 @@ public class UserAuthenticationService {
         if(!userLoginDto.getUsername().equals(userDetails.getUsername())) {
             throw new BadCredentialsException("Invalid username");
         }
-
-        if (!userLoginDto.getPassword().equals(userDetails.getPassword())) {
-            throw new BadCredentialsException("Invalid password");
-        }
         try {
+
+            if (!bCryptPasswordEncoder.matches(userLoginDto.getPassword(), userDetails.getPassword())) {
+                throw new BadCredentialsException("Invalid password");
+            }
+
             Authentication authentication = new JwtAuthentication(userDetails);
 
             log.info("Authenticated user:{} ", userDetails.getUsername());
@@ -44,5 +65,33 @@ public class UserAuthenticationService {
             log.error("Authentication failed for user:{} ", userLoginDto.getUsername(), e);
             throw new BadCredentialsException("Authentication failed for user: " + userLoginDto.getUsername());
         }
+    }
+
+    public UserEntity createUser(@RequestBody UserRegisterDto userRegisterDto) {
+        log.info("Register attempt user {}", userRegisterDto.getUsername());
+        log.info("Register attempt user {}", userRegisterDto.getPassword());
+        log.info("Register attempt user {}", userRegisterDto.getRoles());
+
+        // Create a new user entity
+        UserEntity newUser = new UserEntity();
+        newUser.setUsername(userRegisterDto.getUsername());
+        newUser.setPassword(bCryptPasswordEncoder.encode(userRegisterDto.getPassword()));
+        newUser.setEmail(userRegisterDto.getEmail());
+        newUser.setPhoneNumber(userRegisterDto.getPhoneNumber());
+        newUser.setAddress(userRegisterDto.getAddress());
+        newUser.setBio(userRegisterDto.getBio());
+        newUser.setWebsite(userRegisterDto.getWebsite());
+        newUser.setResumeUrl(userRegisterDto.getResumeUrl());
+        newUser.setCreatedAt(LocalDateTime.now());
+
+        // Validate if the roles exist in the database
+        Optional<RoleEntity> optionalRoleEntity = roleRepository.findByName(userRegisterDto.getRoles());
+        if(optionalRoleEntity.isPresent()) {
+            RoleEntity roleEntity = optionalRoleEntity.get();
+            newUser.setRoles(new HashSet<>(Collections.singletonList(roleEntity)));
+        } else {
+            throw new IllegalArgumentException("Invalid role" + userRegisterDto.getRoles());
+        }
+        return userRepository.save(newUser);
     }
 }
