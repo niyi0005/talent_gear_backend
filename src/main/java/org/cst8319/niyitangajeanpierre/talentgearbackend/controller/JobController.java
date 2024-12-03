08da48dee326174401004f5f9640b692008e08ff
Brandon <brandonnolan20@gmail.com>
2024-12-03 16:25:49 -0500

    Update SecurityConfig to allow requests from front-end server

--- src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/controller/JobController.java
+++ src/main/java/org/cst8319/niyitangajeanpierre/talentgearbackend/controller/JobController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.Dto.JobDto;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.entity.JobEntity;
 import org.cst8319.niyitangajeanpierre.talentgearbackend.service.JobService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/jobs")
 public class JobController {
@@ -37,15 +39,22 @@ public class JobController {
         return jobService.createJob(jobEntity);
     }
 
-   /*  @GetMapping("/{id}")
-    public JobDto getJobById(@PathVariable Long id) {
-
-        return jobService.getJobById(id);
-    } */
+    /*
+     * @GetMapping("/{id}")
+     * public JobDto getJobById(@PathVariable Long id) {
+     * 
+     * return jobService.getJobById(id);
+     * }
+     */
 
     @DeleteMapping("/{id}")
     public void deleteJob(@PathVariable Long id) {
         jobService.deleteJob(id);
+    }
+
+    @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> handleOptions() {
+        return ResponseEntity.ok().build();
     }
 
 }
