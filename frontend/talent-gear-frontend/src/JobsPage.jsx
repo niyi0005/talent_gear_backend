edfc19ddf331fa28225c1948f7b5e13fc1f4d4ad
BrandonNolan117 <nola0082@algonquinlive.com>
2024-12-15 15:02:21 -0500

    Integrate register page with backend. Add jobspage Styling

--- frontend/talent-gear-frontend/src/JobsPage.jsx
+++ frontend/talent-gear-frontend/src/JobsPage.jsx
@@ -1,9 +1,15 @@
-import AllJobs from './components/AllJobs.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AllJobs from './components/AllJobs';
 
 const JobsPage = () => {
-    return  (
+  return (
+    <div style={{ minHeight: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#f8f9fa' }}>
+      <div className="card p-4 bg-primary" style={{ width: '800px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+        <h2 className="text-center text-primary mb-4 text-black">Jobs</h2>
         <AllJobs />
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default JobsPage;
